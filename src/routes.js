8dd52bf903b4f23f0665b6ca7e21bb6cacac002d
Patrick Lima <patrickhl94@hotmail.com>
2020-03-13 01:46:02 -0300

    Create routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import RecipientsController from './app/controllers/RecipientsController';
 import UserAdminController from './app/controllers/UserAdminController';
+import DelivermanController from './app/controllers/DelivermanController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = Router();
@@ -10,6 +11,11 @@ const routes = Router();
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+routes.post('/deliverymans', DelivermanController.store);
+routes.get('/deliverymans', DelivermanController.show);
+routes.get('/deliverymans-all', DelivermanController.index);
+routes.delete('/deliverymans/:id', DelivermanController.destroy);
+routes.put('/deliverymans/:id', DelivermanController.update);
 routes.get('/users-admins', UserAdminController.show);
 routes.post('/recipients', RecipientsController.store);
 routes.put('/recipients', RecipientsController.update);
