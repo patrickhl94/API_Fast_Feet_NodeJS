c2c8e5612eb2e25cc074b80993efd3c465a94e5c
Patrick Lima <patrickhl94@hotmail.com>
2020-03-13 01:49:01 -0300

    Insert models e associate in index

--- src/database/index.js
+++ src/database/index.js
@@ -5,8 +5,10 @@ import databaseConfig from '../config/database';
 // IMPORT MODELS
 import UserAdmin from '../app/models/UserAdmin';
 import Recipients from '../app/models/Recipients';
+import Avatar from '../app/models/Avatar';
+import Deliverman from '../app/models/Deliverman';
 
-const models = [UserAdmin, Recipients];
+const models = [UserAdmin, Recipients, Avatar, Deliverman];
 
 class Database {
   constructor() {
@@ -16,6 +18,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
     models.map(model => model.init(this.connection));
+    models.map(
+      model => model.associate && model.associate(this.connection.models)
+    );
   }
 }
 
