95f6ddaf570e9fc6f08e2e782a22369b49bbc85c
Patrick Lima <patrickhl94@hotmail.com>
2020-03-14 00:29:41 -0300

    Create multer config

---
+++ src/config/multer.js
@@ -0,0 +1,18 @@
+import { resolve, extname } from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+export default {
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'temp', 'uploads'),
+    filename: (req, file, cb) => {
+      crypto.randomBytes(8, (err, res) => {
+        if (err) return cb(err);
+        return cb(null, res.toString('hex') + extname(file.originalname));
+      });
+    },
+  }),
+  /* fileFilter: '',
+  limits: '',
+  preservePath: '', */
+};
