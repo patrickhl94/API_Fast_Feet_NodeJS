e5e6d318a4d5933eea633f9863a248b51ab2ca21
Patrick Lima <patrickhl94@hotmail.com>
2020-03-14 00:31:18 -0300

    Insert VIRTUAL data URL for model Avatar

--- src/app/models/Avatar.js
+++ src/app/models/Avatar.js
@@ -6,6 +6,12 @@ class Avatar extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3300/avatar/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
