f2a4c7dab33b41b34633eef7f181719e2c163709
Patrick Lima <patrickhl94@hotmail.com>
2020-03-15 01:27:16 -0300

    Created config email

---
+++ src/config/mail.js
@@ -0,0 +1,15 @@
+/* Este projeto está sendo utilizado o serviço
+   de email para desenvolvmento: MAILTRAP
+*/
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'afafd439474e28',
+    pass: '57bcd577d82150',
+  },
+  default: {
+    from: 'Equipe Delivery Fast Feet <noreply@fastfeet.com>',
+  },
+};
