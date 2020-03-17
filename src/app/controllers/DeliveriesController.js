1f51820c903787abaa5f6e70c23f4a02020829a0
Patrick Lima <patrickhl94@hotmail.com>
2020-03-16 23:38:08 -0300

    Updated Controller Deliveries

--- src/app/controllers/DeliveriesController.js
+++ src/app/controllers/DeliveriesController.js
@@ -1,4 +1,4 @@
-import { format, startOfHour, parseISO } from 'date-fns';
+import { format } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Deliveries from '../models/Deliveries';
 import Deliverman from '../models/Deliverman';
