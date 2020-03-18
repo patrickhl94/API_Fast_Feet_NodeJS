7ee4c6e0c967185844cf4440b544366bc91a8e58
Patrick Lima <patrickhl94@hotmail.com>
2020-03-17 23:32:29 -0300

    Created Route INDEX in controller ManageDeliverie

--- src/app/controllers/ManageDeliveriesController.js
+++ src/app/controllers/ManageDeliveriesController.js
@@ -1,5 +1,18 @@
 import Deliverman from '../models/Deliverman';
+import Deliveries from '../models/Deliveries';
 
-class ManageDeliveriesController {}
+class ManageDeliveriesController {
+  async index(req, res) {
+    const deliveries = Deliveries.findAll({
+      where: {
+        deliveryman_id: req.params.id,
+        canceled_at: null,
+        end_date: null,
+      },
+    });
+
+    return res.json(deliveries);
+  }
+}
 
 export default new ManageDeliveriesController();
