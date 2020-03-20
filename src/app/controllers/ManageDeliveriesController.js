f2b0fbb1f345bcdc258be02cd6a333f1caae3dcb
Patrick Lima <patrickhl94@hotmail.com>
2020-03-19 21:21:25 -0300

    Updated Manage Deliveries Controller

--- src/app/controllers/ManageDeliveriesController.js
+++ src/app/controllers/ManageDeliveriesController.js
@@ -1,15 +1,25 @@
+import { Op } from 'sequelize';
 import Recipients from '../models/Recipients';
 import Deliveries from '../models/Deliveries';
 
 class ManageDeliveriesController {
   async index(req, res) {
+    const { id, delivered } = req.query;
+    console.log(delivered);
     const deliveries = await Deliveries.findAll({
       where: {
-        deliveryman_id: req.params.id,
+        deliveryman_id: id,
         canceled_at: null,
         end_date: null,
+        // start_date: delivered === 'S' ? !null : null,
+        start_date:
+          delivered === 'S'
+            ? {
+                [Op.ne]: null,
+              }
+            : null,
       },
-      attributes: ['id', 'product'],
+      attributes: ['id', 'product', 'start_date', 'end_date'],
       include: [
         {
           model: Recipients,
